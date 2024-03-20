0efdae9109b9115a53b9c207b35005745f95a093
nannabessa <nannabessa@gmail.com>
2024-03-19 22:26:56 -0300

    Player Spotify

---
+++ script.js
@@ -0,0 +1,31 @@
+const songName = document.getElementById(`song-name`);
+const song = document.getElementById(`audio`);
+const play = document.getElementById(`play`);
+
+songName.innerText = `Mein Herz Brennt`;
+let isPlaying = false;
+
+function playSong(){
+    play.querySelector(`.bi`).classList.remove(`bi-play-circle-fill`)
+    play.querySelector(`.bi`).classList.add(`bi-pause-circle-fill`)
+    song.play();
+    isPlaying = true;
+}
+
+function pauseSong(){
+    play.querySelector(`.bi`).classList.add(`bi-play-circle-fill`)
+    play.querySelector(`.bi`).classList.remove(`bi-pause-circle-fill`)
+    song.pause();
+    isPlaying = false;
+}
+
+function playPauseDecider(){
+    if (isPlaying === true){
+        pauseSong();
+    }
+    else {
+
+    }
+}
+
+play.addEventListener(`click`, playSong);
